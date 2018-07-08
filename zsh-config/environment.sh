53417387477e955054446ff2fae33016cb8b4491
Li Yang <7325560@qq.com>
2018-07-09 02:11:08 +0800

    add scripts dir into path. set 256 color TERM. set perl build cpan mirror. set cpanm mirror.

--- zsh-config/environment.sh
+++ zsh-config/environment.sh
@@ -1,3 +1,14 @@
+# common
+export TERM=xterm-256color 
+export PATH=$HOME/scripts:$PATH
+
+# home brew
 export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
 
-export PATH=$HOME/scripts:$PATH
+# plenv
+export PERL_BUILD_CPAN_MIRROR=http://mirrors.163.com/cpan/
+export PATH="$HOME/.plenv/bin:$PATH"
+if which plenv > /dev/null; then eval "$(plenv init - zsh)"; fi
+
+# cpanm
+alias cpanm="cpanm --mirror http://mirrors.shu.edu.cn/CPAN/"
