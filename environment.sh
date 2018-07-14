d5be635efd2ef0aa3896af09a924572bc99a2f7e
liyang <7325560@qq.com>
2018-07-14 18:01:32 +0800

    add proxy function

--- environment.sh
+++ environment.sh
@@ -11,3 +11,18 @@ if which plenv > /dev/null; then eval "$(plenv init - zsh)"; fi
 
 # cpanm
 alias cpanm="cpanm --mirror http://mirrors.shu.edu.cn/CPAN/"
+
+# proxy
+proxy_vars=('ALL_PROXY' 'all_proxy')
+
+function setproxy() {
+	for var in ${proxy_vars[@]}; do
+		export $var=socks5://127.0.0.1:1080
+	done
+}
+
+function noproxy() {
+	for var in ${proxy_vars[@]}; do
+		unset $var
+	done
+}
