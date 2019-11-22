089743898042a20957bef2c3940c94af7fc7b6c5
liyang <7325560@qq.com>
2019-11-22 10:30:10 +0800

    adjust emacs alias, add cnpm with taobao mirror config

--- alias.sh
+++ alias.sh
@@ -3,8 +3,14 @@ alias cpanm="cpanm --mirror http://mirrors.shu.edu.cn/CPAN/"
 
 # editor
 VIM="/Applications/MacVim.app/Contents/MacOS/Vim"
-EMACS="/Applications/Emacs.app/Contents/MacOS/Emacs -nw"
+EMACS="/Applications/Emacs.app/Contents/MacOS/Emacs"
 
 alias vim=$VIM
 alias vi=$VIM
-alias emacs=$EMACS
+alias emacs="$EMACS -nw"
+alias gemacs=$EMACS
+
+alias cnpm="npm --registry=https://registry.npm.taobao.org \
+--cache=$HOME/.npm/.cache/cnpm \
+--disturl=https://npm.taobao.org/dist \
+--userconfig=$HOME/.cnpmrc"
