b07089803c2e4ba77a64ef89e69274e0e6a3c8c3
liyang <7325560@qq.com>
2020-06-09 09:02:32 +0800

    remove cpanm check vim and emacs app before set alias

--- alias.sh
+++ alias.sh
@@ -1,16 +1,15 @@
-# cpanm
-alias cpanm="cpanm --mirror http://mirrors.163.com/cpan/"
-
 # editor
 VIM="/Applications/MacVim.app/Contents/MacOS/Vim"
 EMACS="/Applications/Emacs.app/Contents/MacOS/Emacs"
 
-alias vim=$VIM
-alias vi=$VIM
-alias emacs="$EMACS -nw"
-alias gemacs=$EMACS
+if [ -e "$VIM" ]
+then
+    alias vim=$VIM
+    alias vi=$VIM
+fi
 
-alias cnpm="npm --registry=https://registry.npm.taobao.org \
---cache=$HOME/.npm/.cache/cnpm \
---disturl=https://npm.taobao.org/dist \
---userconfig=$HOME/.cnpmrc"
+if [ -e "$EMACS" ]
+then
+    alias emacs="$EMACS -nw"
+    alias gemacs=$EMACS
+fi
