3b1166689c6c8af2433c0f09fc340ffc346d9e08
liyang <7325560@qq.com>
2020-11-20 15:42:15 +0800

    修改homebrew的镜像地址 把homebrew的sbin目录加入PATH 设置ruby的openssl库位置 设置electron的镜像地址

--- environment.sh
+++ environment.sh
@@ -1,6 +1,6 @@
 
 # common
-export PATH=$HOME/scripts:$HOME/bin:$PATH
+export PATH="/usr/local/opt/openjdk/bin:/usr/local/sbin:$PATH"
 
 if [ -e "$HOME/.emacs.d/bin" ]
 then
@@ -8,7 +8,7 @@ then
 fi
 
 # homebrew
-export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles
+export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
 
 # cpanm
 export PERL_CPANM_OPT="--mirror http://mirrors.163.com/cpan/"
@@ -25,6 +25,12 @@ if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
 
 # rbenv
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
+
+# ruby
+export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
+
+# electron
+export ELECTRON_MIRROR="https://npm.taobao.org/mirrors/electron/"
 
 # charset to utf8
 export LANG=en_US.UTF-8
