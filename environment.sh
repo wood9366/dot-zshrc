87146508f95e351c441e3b964f760e6fd332e42b
liyang <7325560@qq.com>
2022-06-05 14:17:57 +0800

    update env variables

--- environment.sh
+++ environment.sh
@@ -7,11 +7,19 @@ then
     export PATH=$PATH:$HOME/.emacs.d/bin
 fi
 
+if [ -e "$HOME/.roswell/bin" ]
+then
+    export PATH="$HOME/.roswell/bin:$PATH"
+fi
+
 # homebrew
 export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
 
 # cpanm
 export PERL_CPANM_OPT="--mirror http://mirrors.163.com/cpan/"
+
+# luaver
+if which luaver > /dev/null; then . $(which luaver); fi
 
 # nodenv
 if which nodenv > /dev/null; then eval "$(nodenv init -)"; fi
@@ -38,8 +46,14 @@ export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
 # electron
 export ELECTRON_MIRROR="https://npm.taobao.org/mirrors/electron/"
 
+# dotnet
+export DOTNET_ROOT="/usr/local/opt/dotnet/libexec"
+export PATH="$PATH:$HOME/.dotnet/tools"
+
 # charset to utf8
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
 TERM=xterm-256color
+
+export EDITOR="/Applications/Emacs.app/Contents/MacOS/Emacs"
